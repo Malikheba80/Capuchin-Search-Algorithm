ace721019be4970a6f4fbeab9d1c96359daf35f6
Malikheba80 <80470963+Malikheba80@users.noreply.github.com>
2021-08-21 14:58:37 +0300

    Update CapuchinAlg.m

--- CapuchinAlg.m
+++ CapuchinAlg.m
@@ -1,10 +1,10 @@
-%% CapSA (Capuchin Swarm Algorithm)
+%% CapSA (Capuchin search Algorithm)
 % Citation details:
 % Braik, Malik, Alaa Sheta, and Heba Al-Hiary. "A novel meta-heuristic search algorithm for solving 
-% optimization problems: capuchin search algorithm.
+% optimization problems: Capuchin search algorithm.
 % " Neural Computing and Applications (2020): 1-33
 
-% Programmed by Malik Braik & Prof. Alaa Sheta
+% Programmed by Malik Braik
 % Al-Balqa Applied University (BAU) %
 % Date of programming: 2020 %
 % -------------------------------------------------
@@ -13,136 +13,157 @@
 % -------------------------------------------------	
 % Note:
 % Due to the stochastic nature of meta-heuristc algorithms, 
-% different runs may produce slightly different results.____________________________
+% different runs may produce slightly different results.
 
-function [fFitness,CapuchinPositions,cg_curve]=CapuchinAlg(noP,maxite,LB,UB,dim,fobj)
+function [fitCapSA,CapPos,cg_curve]=CapSA(noP,maxite,LB,UB,dim,fobj)
+ 
 warning off; format long;
+
+%%%%* 1
+% if size(UB,2)==1
+%     UB=ones(1,dim)*UB;
+%     LB=ones(1,dim)*LB;
+% end
+
+ 
+% if size(UB,1)==1
+%     UB=ones(dim,1)*UB;
+%     LB=ones(dim,1)*LB;
+% end
 
 % % % CapSA main program
 % f1 =  figure (1);
 % set(gcf,'color','w');
 % hold on
 % xlabel('Iteration','interpreter','latex','FontName','Times','fontsize',10)
-% ylabel('fitness value','interpreter','latex','FontName','Times','fontsize',10); 
+% ylabel('Fit value','interpreter','latex','FontName','Times','fontsize',10); 
 % grid;
-% %  
+%  
 cg_curve=zeros(1,maxite);
 
 %% % CapSA initialization
 
-%Initialize the positions of Capuchins in the space
-CapuchinPositions=init(noP,dim,UB,LB);
+%Initialize the Pos of Caps in the space
+CapPos=initialization(noP,dim,UB,LB);
 
-v=0.1*CapuchinPositions;% initial velocity
+v=0.1*CapPos;% initial velocity
 v0=zeros(noP,dim); 
+CapFit=zeros(noP,1);
 
-% Calculate the fitness of initialCapuchins
+% Calculate the Fit of initialCaps
 
     for i=1:noP
-     CapuchinFitness(i,1)=fobj(CapuchinPositions(i,:));
+        CapFit(i,1)=fobj(CapPos(i,:));
     end
  
-fitness = CapuchinFitness;
-% Initial fitness of the random positions
+% Initial Fit of the random Pos
 
-[fFitness,index]=min(CapuchinFitness);
+Fit = CapFit;
 
-CapuchinBestPosition = CapuchinPositions; % Best position initialization 
-gFoodPosition = CapuchinPositions(index,:); % initial global position 
+[fitCapSA,index]=min(CapFit);
+
+CapBestPos = CapPos; % Best Pos initialization
+Pos= CapPos; 
+gFoodPos = CapPos(index,:); % initial global Pos 
+
 %% % CapSA Parameters
-bf=0.90;%Balance factor
-cr=19.0;  %Modulus of elasticity
+bf=0.70;%Balance factor
+cr=11.0;  %Modulus of elasticity
 g=9.81;
 
 % CapSA velocity updates
 a1=1.250; a2=1.5;   
 
-beta=[0.05 11 2];
-wmax=0.9;
+beta=[2 11 2];
+wmax=0.8;
 wmin=0.1;
+ 
 %% % CapSA Main loop
-    
+  
 for t = 1 : maxite
 
     % Life time convergence
+        tau = beta(1) * exp(-beta(2) * t/maxite)^beta(3);
+        w   = wmax-(wmax-wmin)*(t/maxite); 
+        fol=ceil((noP-1).*rand(noP,1))'; %  
 
-        tau = beta(1) * exp(-beta(2) * ((t)/maxite)^beta(3));
-        w   = wmax-(wmax-wmin)*(t/maxite)^1; 
- 
         % CapSA velocity update
-     
-    for i=1:noP 
-        for j=1:dim
+for i=1:noP 
+    for j=1:dim
         v(i,j)=  w* v(i,j) + ...
-             1*  a1*(CapuchinBestPosition(i,j)- CapuchinPositions(i,j))*rand + ...
-              1* a2*(gFoodPosition(j) - CapuchinPositions(i,j))*rand; 
- 
-        end        
+                           a1*(CapBestPos(i,j)- CapPos(i,j))*rand + ...
+                           a2*(gFoodPos(j)  - CapPos(i,j))*rand;      
     end
+end
      
-% CapSA position update
+% CapSA Pos update
 
 for i=1:noP
    if i<noP/2
-      for j=1:dim
           if (rand()>=0.1)
                r=rand;
               if r<=0.15
-                 CapuchinPositions(i,j) =  gFoodPosition(j)+    1*bf*((v(i,j).^2)*sin(2*rand()*1.5))/g;  % Jumping (Projection)
+                 CapPos(i,:) =  gFoodPos +    bf*((v(i,:).^2)*sin(2*rand()*1.5))/g;  % Jumping (Projection)
               elseif   r>0.15 && r<=0.30  
-                  CapuchinPositions(i,j) =  gFoodPosition(j)+    1*cr*bf*((v(i,j).^2)*sin(2*rand()*1.5))/g;  % Jumping (Land)  
+                  CapPos(i,:) =  gFoodPos +   cr*bf*((v(i,:).^2)*sin(2*rand()*1.5))/g;  % Jumping (Land)  
               elseif   r>0.30 && r<=0.9      
-                  CapuchinPositions(i,j) =    CapuchinPositions(i,j) +  v(i,j); % Movement on the ground    
+                  CapPos(i,:) =    CapPos(i,:) +  v(i,:); % Movement on the ground    
               elseif  r>0.9 && r<=0.95 
-                 CapuchinPositions(i,j) =      gFoodPosition(j)  +  bf*1.0*sin(1*rand()*1.5);   % Swing % Local search  
+                 CapPos(i,:) =      gFoodPos   +  bf*sin(rand()*1.5);   % Swing % Local search  
               elseif   r>0.95 
-                CapuchinPositions(i,j) =       gFoodPosition(j)  +  bf*1.0*(v(i,j)- v0(i,j));    % Climbing   % Local search
+                CapPos(i,:) =       gFoodPos   +  bf*(v(i,:)- v0(i,:));    % Climbing   % Local search
               end
-        else
-            CapuchinPositions(i,j) =             tau*(LB(j)  + rand *(UB(j)    - LB(j))); 
-        end      
-    end
-% Let the followers follow the leaders (update their positions)
+          else
+               CapPos(i,:) =           tau*(LB  + rand *(UB- LB));   
+      end
+    
+% Let the followers follow the leaders (update their Pos)
 elseif i>=noP/2 && i<=noP 
-        alphaPos=CapuchinPositions(i-1,:);
-        followerPos=CapuchinPositions(i,:);
-        CapuchinPositions(i,:)=(alphaPos+followerPos)/2; 
+            
+           eps=((rand()+2*rand())-(3*rand()))/(1+rand()); 
+     
+           Pos(i,:)=gFoodPos+2*(CapBestPos(fol(i),:)-CapPos(i,:))*eps +...
+                                 2*(CapPos(i,:)-CapBestPos(i,:))*eps;      
+ 
+          CapPos(i,:)=(Pos(i,:)+ CapPos(i-1,:))/(2); 
+    
    end
 end 
-v0 = v;
+v0 = v;   
+ 
 
 for i=1:noP % relocation (Update, exploration)
-        u=CapuchinPositions(i,:)>UB;
-        l=CapuchinPositions(i,:)<LB;
+        u=UB-CapPos(i,:)<0;
+        l=LB-CapPos(i,:)>0;
      
-         CapuchinPositions(i,:)= (CapuchinPositions(i,:).*~xor(u,l))+UB.*u+LB.*l;
+         CapPos(i,:)= LB.*l+UB.*u+CapPos(i,:).*~xor(u,l);
     
-         CapuchinFitness(i,1)=fobj (CapuchinPositions(i,:)) ;
+         CapFit(i,1)=fobj (CapPos(i,:)) ;
          
-            if CapuchinFitness(i,1)<fitness(i)
-                CapuchinBestPosition(i,:)=CapuchinPositions(i,:);
-                fitness(i)=CapuchinFitness(i,1);
+            if CapFit(i,1)<Fit(i)
+                CapBestPos(i,:)=CapPos(i,:);
+                Fit(i)=CapFit(i,1);
+                
             end 
 end
-%% Evaluate the new positions
+%% Evaluate the new Pos
 
-[fmin,index]=min(fitness); % finding out the best positions  
+[fmin,index]=min(Fit); % finding out the best Pos  
 
-% Updating gPosition and best fitness
-if fmin < fFitness
-    gFoodPosition = CapuchinBestPosition(index,:); % Update the global best positions
-    fFitness = fmin;
+% Updating gPos and best Fit
+if fmin < fitCapSA
+    gFoodPos = CapBestPos(index,:); % Update the global best Pos
+    fitCapSA = fmin;
 end
 
-% % %     % Display the iterative results
+% %     % Display the iterative results
 % 
-%      outmsg = ['Iteration# ', num2str(t) , '  Fitness= ' , num2str(fFitness)];
-%         disp(outmsg);
-% % %    
-% 
-%    % Obtain the convergence curve
-     cg_curve(t)=fFitness;
-%      
+%          disp(['Iteration# ', num2str(t) , '  Fit= ' , num2str(fitCapSA)]);
+% %    
+
+   % Obtain the convergence curve
+     cg_curve(t)=fitCapSA;
+     
 %  if t>2
 %      set(0, 'CurrentFigure', f1)
 % 
@@ -151,8 +172,8 @@ end
 %         ylabel('Best score obtained so far');
 %         drawnow 
 %  end
+%   
    
 end
-
-fFitness =fobj(gFoodPosition);
+fitCapSA =fobj(gFoodPos);
 end
